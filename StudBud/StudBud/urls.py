f4f7a926e000acf0dee113e8854e8f80ae6b3c30
Parth Sitoot <parth.sitoot@gmail.com>
2025-12-24 09:17:15 +0530

    Adding files

--- StudBud/StudBud/urls.py
+++ StudBud/StudBud/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse("Welcome to home page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('base.urls')),
 ]
--- StudBud/base/views.py
+++ StudBud/base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
+def home(request):
+    return render(request, 'base/home.html')
+
+def room(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/room.html', context)
