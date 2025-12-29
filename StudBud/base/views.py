4e7a4ead888b343dad452db05359485605045a3e
Parth Sitoot <parth.sitoot@gmail.com>
2025-12-29 12:11:09 +0530

    Adding files

--- StudBud/base/views.py
+++ StudBud/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 rooms = [
     {'id': 1, 'name': 'Room 1'},
@@ -7,9 +7,13 @@ rooms = [
     {'id': 3, 'name': 'Room 3'},
 ]
 
-def home(request):
-    return render(request, 'base/home.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
 
-def room(request):
-    context = {'rooms': rooms}
+    context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
