c4ffcc07f73aac09ad93f13cc97828299ef9d482
Parth Sitoot <parth.sitoot@gmail.com>
2026-01-02 19:30:48 +0530

    Adding features

--- StudBud/base/forms.py
+++ StudBud/base/forms.py
@@ -1,10 +1,12 @@
 from django.forms import ModelForm
-from .models import Room
+from .models import Room, Topic
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
 
-    
-
+class TopicForm(ModelForm):
+    class Meta:
+        model = Topic
+        fields = '__all__'
--- StudBud/base/views.py
+++ StudBud/base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Room
-from .forms import RoomForm
+from .forms import RoomForm, TopicForm
 
 # Create your views here.
 rooms = [
@@ -23,8 +23,40 @@ def home(request):
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
-        print(request.POST)
-
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
-    
+
+def createTopic(request):
+    form = TopicForm()
+    if request.method == 'POST':
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form' : form}
+    return render(request, 'base/topic_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form' : form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
